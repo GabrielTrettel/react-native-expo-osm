0c6224e045d70914626889e4426adf2106c5c163
GabrielTrettel <gabrielmtrettel@gmail.com>
2021-05-07 13:50:25 -0300

    initial google maps view

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+import { Dimensions } from "react-native";
 
 export default function App() {
+  const screen_width = Dimensions.get("window").width;
+  const screen_height = Dimensions.get("window").height;
+  const default_location = {
+    latitude: -23.644957,
+    longitude: -46.528012,
+    latitudeDelta: 0.02,
+    longitudeDelta: 0.02 * (screen_width / screen_height),
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.mapStyle}
+        showsUserLocation={true}
+        initialRegion={{ ...default_location }}
+      ></MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "black",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  mapStyle: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
