e5b83333486c7033eb2df19a79bf0546f6d7b5a2
GabrielTrettel <gabrielmtrettel@gmail.com>
2021-05-26 17:25:45 -0300

    Load HTML from .html file

--- src/MapView.js
+++ src/MapView.js
@@ -1,6 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { View } from "react-native";
 import WebView from "react-native-webview";
-import { map } from "./Map.js";
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+
+const HTML_FILE_PATH = require(`./Map.html`);
+
+const loadHTMLFile = async () => {
+  try {
+    const [{ localUri }] = await Asset.loadAsync(HTML_FILE_PATH);
+    const fileString = await FileSystem.readAsStringAsync(localUri);
+
+    return fileString;
+  } catch (error) {
+    console.warn(error);
+    console.warn("Unable to resolve index file");
+  }
+};
 
 const code_to_function = {
   "1": clickCallback,
@@ -35,6 +51,11 @@ function goToPosition(mapRef, lat, long) {
 export default function MapView(props) {
   const [mapRef, setMapRef] = useState(null);
   const [finishedLoad, setFinishedLoad] = useState(false);
+  const [webviewContent, setWebviewContent] = useState(null);
+
+  loadHTMLFile()
+    .then((html) => setWebviewContent(html))
+    .catch((e) => console.warn(e));
 
   props.animateToPosition != null &&
     goToPosition(mapRef, ...props.animateToPosition);
@@ -49,19 +70,22 @@ export default function MapView(props) {
     mapRef != null && onLoad();
   }, [finishedLoad]);
 
+  console.log(webviewContent);
   return (
-    <WebView
-      ref={(webViewRef) => {
-        setMapRef(webViewRef);
-      }}
-      onMessage={(event) => {
-        parseInput(event.nativeEvent.data, props);
-      }}
-      javaScriptEnabled={true}
-      source={{ html: map }}
-      onLoad={() => {
-        setFinishedLoad(true);
-      }}
-    />
+    <View flex={1}>
+      <WebView
+        ref={(webViewRef) => {
+          setMapRef(webViewRef);
+        }}
+        onMessage={(event) => {
+          parseInput(event.nativeEvent.data, props);
+        }}
+        javaScriptEnabled={true}
+        source={{ html: webviewContent }}
+        onLoad={() => {
+          setFinishedLoad(true);
+        }}
+      />
+    </View>
   );
 }
